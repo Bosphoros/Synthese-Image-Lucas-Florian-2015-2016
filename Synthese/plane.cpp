731558506a9c76c622c156fc7684229e9fe72cb4
Bosphoros <floriantd.adam@gmail.com>
2015-10-18 14:34:29 +0200

    Correction intersection plan : abs qui était int

--- Synthese/plane.cpp
+++ Synthese/plane.cpp
@@ -8,8 +8,8 @@ Plane::Plane(const Vector3D &n, const Vector3D &p) : normal(n), point(p)
 Plane::Plane(const Vector3D &a, const Vector3D &b, const Vector3D &c) : point(a)
 {
     // Vecteurs du plan
-    Vector3D u(a - c);
-    Vector3D v(b - c);
+    Vector3D u(c - a);
+    Vector3D v(b - a);
     normal = u^v;
 }
 
@@ -17,7 +17,8 @@ bool Plane::intersects(const Ray &r, Vector3D &out)
 {
     // Produit scalaire, si 0 pas de solution ou une infinité
     double scalaire = r.getDirection()*normal;//Vector3D::dotProduct(direction, w); //direction.x()*w.x()+direction.y()*w.y()+direction.z()*w.z();
-    if(abs(scalaire) < 0.0001)
+
+    if(std::abs(scalaire) < 0.0001)
         return false;
     double delta = point*normal;//Vector3D::dotProduct(a, w);
 
